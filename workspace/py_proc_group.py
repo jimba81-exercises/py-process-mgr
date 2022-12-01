adcf977621ba1815b13a41d47d032b8cdf6188a8
James Yoo <jimba81yoo@gmail.com>
2022-12-02 09:19:27 +1100

    Process sequential/concurrent support

--- workspace/py_proc_group.py
+++ workspace/py_proc_group.py
@@ -3,68 +3,64 @@ from typing import Any, List, Union
 from abc import ABC, abstractmethod
 from py_proc import PyProc
 
+
 class PyProcGroup(ABC):
-  def __init__(self, proc_groups: List[Any]):
-    self.proc_groups = proc_groups
+  def __init__(self, proc_items: List[Any]):
+    self.proc_items = proc_items
 
   @abstractmethod
-  def start(self, shared_np):
+  def start(self, shared_np) -> None:
     pass
 
+  @abstractmethod
+  def join(self) -> None:
+    pass  
+
+
+# Process Item
+ProcItem = Union[PyProcGroup, PyProc]
 
 class PyProcGroup_Sequential(PyProcGroup):
-  def __init__(self, proc_groups: List[Union[PyProcGroup, PyProc]]):
-    super().__init__(proc_groups)
+  def __init__(self, proc_items: List[ProcItem]):
+    super().__init__(proc_items)
 
-  def start(self, shared_np):
-    print(f"{self.__class__.__name__}: run(): START")    
+  def start(self, shared_np) -> None:
+    print(f"{self.__class__.__name__}: start(): START")    
 
-    with multiprocessing.Manager() as proc_mgr:
-      # Define shared namespace
-      # namespace = proc_mgr.Namespace(
-      #   digest = 123
-      # )
-      
-      for group_idx in range(len(self.proc_groups)):
-        proc_group = self.proc_groups[group_idx]
+    for proc_idx in range(len(self.proc_items)):
+      proc_item = self.proc_items[proc_idx]
+      proc_item.start(shared_np)
+      # Current group is sequential type. Wait for each process to complete
+      proc_item.join()
 
-        if isinstance(proc_group, PyProcGroup):
-          print(f"{self.__class__.__name__}: ProcGroup type is PyProcGroup")
-          proc_group.start(shared_np)
-          proc_group.join()
+    print(f"{self.__class__.__name__}: start(): END")    
 
-          # if isinstance(proc_group, PyProcGroup_Sequential):
-          #   print(f"{self.__class__.__name__}: ProcGroup type is PyProcGroup_Sequential")
-          #   proc_group.run()
 
-          # elif isinstance(proc_group, PyProcGroup_Concurrent):
-          #   print(f"{self.__class__.__name__}: ProcGroup type is PyProcGroup_Concurrent")
-          # else:
-          #   raise TypeError("proc_group type is unexpected")
-          
-        elif isinstance(proc_group, PyProc):
-          print(f"{self.__class__.__name__}: ProcGroup type is PyProc")
-          proc_group.start(shared_np)
-          proc_group.join()
-          # print(f"{self.__class__.__name__}: run(): proc[{group_idx}]: started")    
-          # process = multiprocessing.Process(target=proc_group.task, args=(namespace,))
-          # process.start()
-          # process.join()
-          # print(f"{self.__class__.__name__}: run(): proc[{group_idx}]: ended")    
-          
-        else:
-          raise TypeError("proc_group type is unexpected")
-
-  def join(self):
+  def join(self) -> None:
+    # Current group is sequential type. Nothing to wait.
     return
-
-    print(f"{self.__class__.__name__}: run(): END")    
-    
 
 
 class PyProcGroup_Concurrent(PyProcGroup):
-  def __init__(self, proc_groups: List[PyProcGroup | PyProc]):
-    super().__init__(proc_groups)
+  def __init__(self, proc_items: List[ProcItem]):
+    super().__init__(proc_items)
 
-  def run(self):
-    pass
+  def start(self, shared_np) -> None:
+    print(f"{self.__class__.__name__}: start(): START")    
+
+    # Current group is concurrent type. Start all processes concurrently
+    for proc_idx in range(len(self.proc_items)):
+      proc_item = self.proc_items[proc_idx]
+      proc_item.start(shared_np)
+
+
+
+    print(f"{self.__class__.__name__}: start(): END")    
+
+
+  def join(self) -> None:
+    # Wait for all processes complete
+    for proc_idx in range(len(self.proc_items)):
+      proc_item = self.proc_items[proc_idx]
+      proc_item.join()
+    return
--- workspace/sample.py
+++ workspace/sample.py
@@ -1,5 +1,5 @@
 import time
-from py_proc_group import PyProc, PyProcGroup_Sequential
+from py_proc_group import PyProc, PyProcGroup_Concurrent, PyProcGroup_Sequential
 from py_proc_mgr import PyProcMgr
 
 
@@ -10,13 +10,13 @@ class MyTask1(PyProc):
 
   def task(self, shared_ns):
     print(f"{self.__class__.__name__}: Task started")
-    time.sleep(1.0)
+    time.sleep(3.0)
     print(f"{self.__class__.__name__}: Task Running - 1")
-    time.sleep(1.0)
+    time.sleep(3.0)
     print(f"{self.__class__.__name__}: Task Running - 2")
-    time.sleep(1.0)
+    time.sleep(3.0)
     print(f"{self.__class__.__name__}: Task Running - 3")
-    time.sleep(1.0)
+    time.sleep(3.0)
     print(f"{self.__class__.__name__}: Task ended")
 
 
@@ -34,6 +34,47 @@ class MyTask2(PyProc):
     time.sleep(1.5)
     print(f"{self.__class__.__name__}: Task ended")
 
+class MyTask3(PyProc):
+  def __init__(self):
+    super().__init__()
+    ...
+
+  def task(self, shared_ns):
+    print(f"{self.__class__.__name__}: Task started")
+    time.sleep(0.5)
+    print(f"{self.__class__.__name__}: Task Running - 1")
+    time.sleep(0.5)
+    print(f"{self.__class__.__name__}: Task Running - 2")
+    time.sleep(0.5)
+    print(f"{self.__class__.__name__}: Task ended")
+
+class MyTask4(PyProc):
+  def __init__(self):
+    super().__init__()
+    ...
+
+  def task(self, shared_ns):
+    print(f"{self.__class__.__name__}: Task started")
+    time.sleep(0.5)
+    print(f"{self.__class__.__name__}: Task Running - 1")
+    time.sleep(0.5)
+    print(f"{self.__class__.__name__}: Task Running - 2")
+    time.sleep(0.5)
+    print(f"{self.__class__.__name__}: Task ended")
+
+class MyTask5(PyProc):
+  def __init__(self):
+    super().__init__()
+    ...
+
+  def task(self, shared_ns):
+    print(f"{self.__class__.__name__}: Task started")
+    time.sleep(0.2)
+    print(f"{self.__class__.__name__}: Task Running - 1")
+    time.sleep(0.2)
+    print(f"{self.__class__.__name__}: Task Running - 2")
+    time.sleep(0.2)
+    print(f"{self.__class__.__name__}: Task ended")
 
 class Main:
   def __init__(self):
@@ -41,6 +82,9 @@ class Main:
 
     my_task1 = MyTask1()
     my_task2 = MyTask2()
+    my_task3 = MyTask3()
+    my_task4 = MyTask4()
+    my_task5 = MyTask5()
 
     self.proc_mgr.set(
       PyProcGroup_Sequential([
@@ -50,6 +94,20 @@ class Main:
           my_task2
         ]),
         my_task1
+      ])
+    )
+
+    self.proc_mgr.set(
+      PyProcGroup_Concurrent([
+        my_task1,
+        PyProcGroup_Sequential([
+          my_task2,
+          my_task3,
+          PyProcGroup_Concurrent([
+            my_task4,
+            my_task5,
+        ]),
+        ]),
       ])
     )
 
